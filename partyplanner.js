a000fd19cf74cc9cd0089d8dcb6033267d79ee3a
Scott Cousins <scousins1024@yahoo.com>
2023-10-03 15:12:16 -0500

    /event endpoint URL

---
+++ partyplanner.js
@@ -0,0 +1,17 @@
+// API_URL
+const API_URL =
+  'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309-FSA-ET-WEB-FT-SF/events';
+
+// function to fetch the parties
+
+// function to render the list of parties
+
+// fetch and render list of parties
+
+//get parties first then render new list of parties
+
+// function to create a new party
+
+// async function with the event handler with .addEventListner call
+
+//function to update existing parties list
